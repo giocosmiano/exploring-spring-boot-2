320b84a936b947ee3e551b27f9d63ef11a3f997e
u0164744 <giocosmiano@gmail.com>
2019-06-01 21:28:04 -0400

    Refactoring application name

--- ch01-quick-start/src/test/java/com/giocosmiano/exploration/chapter01/QuickStartApplicationTests.java
+++ ch01-quick-start/src/test/java/com/giocosmiano/exploration/chapter01/QuickStartApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ExploreSpringBootApplicationTests {
+public class QuickStartApplicationTests {
 
 	@Test
 	public void contextLoads() {
