d8ce11a00ec4202354f55f3d7769e2f14aaab674
u0164744 <giocosmiano@gmail.com>
2019-06-25 19:57:50 -0400

    Spring cloud - Netflix Hystrix Dashboard

--- ch07-micro-service-hystrix-dashboard/src/main/java/com/giocosmiano/exploration/chapter07/SpringCloudHystrixDashboardApplication.java
+++ ch07-micro-service-hystrix-dashboard/src/main/java/com/giocosmiano/exploration/chapter07/SpringCloudHystrixDashboardApplication.java
@@ -7,6 +7,13 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 @EnableHystrixDashboard
 public class SpringCloudHystrixDashboardApplication {
+
+    /*
+     @SpringBootApplication declares this to be a Spring Boot application. We don't need, because
+     we don't intend to hook into Eureka, nor institute any circuit @SpringCloudApplication breakers
+
+     @EnableHystrixDashboard will start up a UI
+     */
     public static void main(String[] args) {
         new SpringApplicationBuilder(SpringCloudHystrixDashboardApplication.class).run(args);
     }
