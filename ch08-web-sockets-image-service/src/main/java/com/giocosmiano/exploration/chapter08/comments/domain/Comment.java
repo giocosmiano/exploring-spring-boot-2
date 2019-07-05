57b4c7b471f3cc4db61dae54d296ba239bec57b5
u0164744 <giocosmiano@gmail.com>
2019-07-04 20:50:57 -0400

    Web Sockets - Image micro service

--- ch08-web-sockets-image-service/src/main/java/com/giocosmiano/exploration/chapter08/comments/domain/Comment.java
+++ ch08-web-sockets-image-service/src/main/java/com/giocosmiano/exploration/chapter08/comments/domain/Comment.java
@@ -1,14 +1,10 @@
 package com.giocosmiano.exploration.chapter08.comments.domain;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
-@Document
 public class Comment {
 
-    @Id
     private String id;
     private String imageId;
     private String comment;
